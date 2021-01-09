2ddbe3e835d67a0c41ec46e9012d073c782c382d
limkimhuor <kimhuorlim@gmail.com>
2021-01-09 22:40:55 +0700

    Config build gradle

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,6 +6,14 @@ plugins {
     id("io.spring.dependency-management") version "1.0.10.RELEASE"
     kotlin("jvm") version kotlinVersion
     kotlin("plugin.spring") version kotlinVersion
+    // annotated with the all-open meta-annotation
+    kotlin("plugin.allopen") version kotlinVersion
+}
+
+allOpen {
+    annotation("javax.persistence.Entity")
+    annotation("javax.persistence.Embeddable")
+    annotation("javax.persistence.MappedSuperclass")
 }
 
 group = "com.hlk"
@@ -18,11 +26,30 @@ repositories {
 
 dependencies {
     implementation("org.springframework.boot:spring-boot-starter-web")
+    // Adds support for serialization/deserialization classes and data classes
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
+    // Introspecting the structure of your own program at runtime
     implementation("org.jetbrains.kotlin:kotlin-reflect")
     implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+
+    // Spring Security OAuth
+    implementation("org.springframework.security.oauth:spring-security-oauth2:2.4.0.RELEASE")
+
+    // Additional development-time features
     developmentOnly("org.springframework.boot:spring-boot-devtools")
-    testImplementation("org.springframework.boot:spring-boot-starter-test")
+
+    // Database
+    implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+    runtimeOnly("mysql:mysql-connector-java")
+
+    // Test API
+    testImplementation("org.springframework.boot:spring-boot-starter-test") {
+        exclude(module = "junit")
+        exclude(module = "mockito-core")
+    }
+    testImplementation("org.junit.jupiter:junit-jupiter-api")
+    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine")
+    testImplementation("com.ninja-squad:springmockk:1.1.3")
 }
 
 tasks.withType<KotlinCompile> {
